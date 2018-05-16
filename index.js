3eceb5ce630af25ff2a174008d797409d4f47add
DavidsMartinez <dasmartineztri@unal.edu.co>
2018-05-16 17:28:43 -0400

    Rebuild

---
+++ index.js
@@ -0,0 +1,13 @@
+
+(function () {
+    document.getElementById("btn").addEventListener("click", function () {
+        alert("Hi! :)");
+    });
+     (script async defer{
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCJZZ_2s2Yuq02EsL3WH6lKQSP466EwvtQ&callback=initMap"
+    (/script)
+    }
+    
+    
+    
+})();
